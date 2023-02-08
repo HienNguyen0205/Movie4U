0aa57aebc7784c7efd1a8f7a57d25af5c847b7a6
Nguyễn Công Hiền <89438952+HienNguyen0205@users.noreply.github.com>
2023-02-08 20:58:03 +0700

    Update 1
    UI header + navbar + carousel

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('test', {layout: 'mainLayout'})
+    res.render('ticket', {
+        layout: 'mainLayout',
+        carousel: [
+            'images/Carousel/Film1.jpg',
+            'images/Carousel/Film2.jpg',
+            'images/Carousel/Film3.jpg',
+        ]
+    })
 })
 
 module.exports = router
