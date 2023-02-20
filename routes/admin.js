6405386a1d9389bfed4500a1e88305eb1559cb2e
maidzui123 <maiduy190802@gmail.com>
2023-02-20 13:04:51 +0700

    upd dashboard main

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,9 @@ const router_admin = express.Router()
 router_admin.get('/', (req, res) => {
     res.render('home_admin', {
         layout: 'mainLayout_admin',
-        
+        icons_dashboard: [
+            {src: './images/Admin/total_views_icon.png', ammount: '645,948'}
+        ]
     })
 })
 
