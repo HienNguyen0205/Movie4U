3434b463f27866a8dc52efdb4c9838d4e595f76f
maidzui123 <maiduy190802@gmail.com>
2023-03-02 11:00:46 +0700

    chart upd

--- public/js/main_admin.js
+++ public/js/main_admin.js
@@ -35,34 +35,73 @@
 
 
 const ctx = document.getElementById('dashboard-chart');
-
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [20, 40, 60, 70, 80, 100],
-        colorText: "#FFFFFF",
-        borderWidth: 1,
-        borderColor: "#9D8CEF",
-        backgroundColor: "#485296"
-      }]
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: MONTHS,
+    datasets: [{
+      label: null,
+      data: [2000, 40000, 55000, 35000, 10000, 14000, 14000, 24000, 34000, 26000, 13000, 44000],
+      fill: true,
+      borderWidth: 1,
+      borderColor: "#9D8CEF",
+      backgroundColor: "rgba(224, 153, 244, 0.5)"
+    }]
+  },
+  options: {
+    animations: {
+      tension: {
+        duration: 1000,
+        esing: 'linear',
+        from: 1,
+        to: 0.5,
+      }
     },
-    options: {
-        animations: {
-            tension: {
-              duration: 1000,
-              esing: 'linear',
-              from: 1,
-              to: 0.5,
-            }
-          },
-          scales: {
-            y: { 
-              min: 0,
-              max: 100
-            }
+    scales: {
+      x: {
+        ticks: {
+          color: '#9D8CEF'
+        }
+      },
+      y: {
+        min: 0,
+        max: 60000,
+        ticks: {
+          color: '#9D8CEF',
+        }
+      }
+    },
+    plugins: {
+      legend: {
+          display: false,
+          labels: {
+              // This more specific font property overrides the global property
+              font: {
+                  size: 18,
+              }
           }
+      },
+      tooltips: {
+        callbacks: {
+           label: function(tooltipItem) {
+                  return tooltipItem.yLabel;
+           }
+        }
+      }
     }
-  });
+  }
+});
