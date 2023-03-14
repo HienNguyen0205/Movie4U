111fc15cecfc205bbcaf394a45a10d42252cd43f
maidzui123 <maiduy190802@gmail.com>
2023-03-14 23:23:09 +0700

    small upd

--- routes/admin.js
+++ routes/admin.js
@@ -5,11 +5,20 @@ router_admin.get('/', (req, res) => {
     res.render('home_admin', {
         layout: 'mainLayout_admin',
         most_popular: [
-            {src: 'images/Movie/aladdin.jpg', name: 'Aladdin', duration: '120 min'},
+            {src: 'images/Movie/aladdin.jpg', name: 'Aladindin', duration: '120 min'},
             {src: 'images/Movie/avenger-endgame.jpg', name: 'Avenger Endgame', duration: '120 min'},
             {src: 'images/Movie/captain-marvel.jpg', name: 'Captain Marvel', duration: '120 min'},
         ],
     })
 })
-
+router_admin.get('/movie_theatres',(req, res) => {
+    res.render('home_admin', {
+        layout: 'mainLayout_admin',
+        most_popular: [
+            {src: 'images/Movie/aladdin.jpg', name: 'Aladindin', duration: '120 min'},
+            {src: 'images/Movie/avenger-endgame.jpg', name: 'Avenger Endgame', duration: '120 min'},
+            {src: 'images/Movie/captain-marvel.jpg', name: 'Captain Marvel', duration: '120 min'},
+        ],
+    })
+})
 module.exports = router_admin
