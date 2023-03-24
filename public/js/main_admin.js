efb8ad836c7b2cbfb0c842747534d868f5653d17
maidzui123 <maiduy190802@gmail.com>
2023-03-24 22:57:03 +0700

    big upd

--- public/js/main_admin.js
+++ public/js/main_admin.js
@@ -1,6 +1,12 @@
 // // Chart Dashboard
-
 const ctx = document.getElementById('dashboard-chart');
+let currChart = [];
+const chartToday = [20000, 40000, 55000, 35000, 10000, 14000, 14000, 24000, 34000, 26000, 13000, 44000];
+const chartWeek = [10000, 55000, 40000, 25000, 35000, 36000, 33000, 24000, 26000, 18000, 28000, 30000];
+const chartMonth = [57000, 24000, 12000, 45000, 10000, 34000, 28000, 35000, 43000, 13000, 30000, 29000];
+const chartYear = [45000, 56000, 34000, 57000, 23000, 45000, 23000, 12000, 18000, 55000, 34000, 48000];
+currChart = [...chartToday];
+console.log(currChart)
 const MONTHS = [
   'January',
   'February',
@@ -15,13 +21,13 @@ const MONTHS = [
   'November',
   'December'
 ];
-new Chart(ctx, {
+const chartDashboard = new Chart(ctx, {
   type: 'line',
   data: {
     labels: MONTHS,
     datasets: [{
       label: null,
-      data: [20000, 40000, 55000, 35000, 10000, 14000, 14000, 24000, 34000, 26000, 13000, 44000],
+      data: currChart,
       fill: true,
       borderWidth: 1,
       borderColor: "#9D8CEF",
@@ -55,7 +61,6 @@ new Chart(ctx, {
       legend: {
           display: false,
           labels: {
-              // This more specific font property overrides the global property
               font: {
                   size: 18,
               }
@@ -72,7 +77,6 @@ new Chart(ctx, {
   }
 });
 
-
 // Time dashboard click 
 
 function timeDashboardUnclick(x) {
@@ -84,27 +88,96 @@ function timeDashboardClick(x) {
   x.style.color = '#cccccc'
 }
 
-document.getElementById("today").onclick = function() {
+function todayClick() {
+  currChart = [...chartToday]
+  chartDashboard.data.datasets[0].data = [...currChart];
+  chartDashboard.update();
   timeDashboardUnclick(document.getElementById("today"))
   timeDashboardClick(document.getElementById("week"))
   timeDashboardClick(document.getElementById("month"))
   timeDashboardClick(document.getElementById("year"))
 };
-document.getElementById("week").onclick = function() {
+function weekClick() {
+  currChart = [...chartWeek]
+  console.log(chartDashboard.data.datasets[0].data)
+  chartDashboard.data.datasets[0].data = [...currChart];
+  chartDashboard.update();
   timeDashboardUnclick(document.getElementById("week"))
   timeDashboardClick(document.getElementById("today"))
   timeDashboardClick(document.getElementById("month"))
   timeDashboardClick(document.getElementById("year"))
 };
-document.getElementById("month").onclick = function() {
+function monthClick() {
+  currChart = [...chartMonth]
+  chartDashboard.data.datasets[0].data = [...currChart];
+  chartDashboard.update();
   timeDashboardUnclick(document.getElementById("month"))
   timeDashboardClick(document.getElementById("week"))
   timeDashboardClick(document.getElementById("today"))
   timeDashboardClick(document.getElementById("year"))
 };
-document.getElementById("year").onclick = function() {
+function yearClick() {
+  currChart = [...chartYear]
+  chartDashboard.data.datasets[0].data = [...currChart];
+  chartDashboard.update();
   timeDashboardUnclick(document.getElementById("year"))
   timeDashboardClick(document.getElementById("week"))
   timeDashboardClick(document.getElementById("today"))
   timeDashboardClick(document.getElementById("month"))
 };
+
+
+
+// Change URL 
+window.onload = function(){ 
+  document.getElementById("dashboard_admin").addEventListener("click", () => {
+    changeUrl("Dashboard")
+  });
+  
+  document.getElementById("movieTheatres_admin").addEventListener("click", () => {
+    changeUrl("MovieTheatres")
+  });
+  document.getElementById("movies_admin").addEventListener("click", () => {
+    changeUrl("Movies")
+  });
+};
+
+// document.getElementById("movies_admin").onclick = changeUrl("Movies");
+// document.getElementById("showTimings_admin").onclick = changeUrl("ShowTiming");
+// document.getElementById("homePage_admin").onclick = changeUrl("HomePage");
+// document.getElementById("users_admin").onclick = changeUrl("Users");
+// document.getElementById("booking_admin").onclick = changeUrl("Booking");
+// document.getElementById("expense_admin").onclick = changeUrl("Expense");
+function changeUrl(str) {
+  const href = window.location.href
+  const currUrl = href.slice(0, href.lastIndexOf('/'))
+  window.location.href = currUrl + '/' + str
+  console.log(str)
+}
+
+// Add movie theatre
+let btnAddTheatre = document.getElementById('btn_add-theatre');
+let theatresList = document.getElementById('list_theatres')
+
+let theatreName = document.getElementById('theatre_name')
+let theatreImg = document.getElementById('theatre_img')
+let count = 0;
+
+btnAddTheatre.addEventListener('click', () => {
+  count++;
+  let name = theatreName.value
+  let img = theatreImg.value.slice(12)
+  console.log(img)
+  let template = `
+            <tr>
+                <th scope="row">${count}</th>
+                <td>${name}</td>
+                <td><img class="theatres_img" src="/images/Movie Theatres/${img}" alt=""></td>
+                <td><label class="theatres_checkbox-outer"><input type="checkbox" id="theatre1"
+                            class="theatres_checkbox" name="theatre1" value="${name}"><span
+                            class="checkmark"></span></label></td>
+                <td>Edit</td>
+            </tr>
+  `
+  theatresList.innerHTML += template
+})
--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,19 @@
 const express = require('express')
 const router_admin = express.Router()
 
-router_admin.get('/', (req, res) => {
+router_admin.get('/Dashboard', (req, res) => {
     res.render('home_admin', {
         layout: 'mainLayout_admin',
         most_popular: [
-            {src: 'images/Movie/aladdin.jpg', name: 'Aladindin', duration: '120 min'},
-            {src: 'images/Movie/avenger-endgame.jpg', name: 'Avenger Endgame', duration: '120 min'},
-            {src: 'images/Movie/captain-marvel.jpg', name: 'Captain Marvel', duration: '120 min'},
+            {src: '/images/Movie/aladdin.jpg', name: 'Aladindin', duration: '120 min'},
+            {src: '/images/Movie/avenger-endgame.jpg', name: 'Avenger Endgame', duration: '120 min'},
+            {src: '/images/Movie/captain-marvel.jpg', name: 'Captain Marvel', duration: '120 min'},
         ],
     })
 })
-router_admin.get('/movie_theatres',(req, res) => {
-    res.render('home_admin', {
-        layout: 'mainLayout_admin',
-        most_popular: [
-            {src: 'images/Movie/aladdin.jpg', name: 'Aladindin', duration: '120 min'},
-            {src: 'images/Movie/avenger-endgame.jpg', name: 'Avenger Endgame', duration: '120 min'},
-            {src: 'images/Movie/captain-marvel.jpg', name: 'Captain Marvel', duration: '120 min'},
-        ],
+router_admin.get('/MovieTheatres',(req, res) => {
+    res.render('movieTheatres_admin', {
+        layout: 'mainLayout_admin'
     })
 })
 module.exports = router_admin
