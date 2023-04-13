19eb2a8ba70cae7ea557cda43705798345e8cf53
maidzui123 <maiduy190802@gmail.com>
2023-04-13 22:43:22 +0700

    small upd

--- config/db/index.js
+++
@@ -1,9 +0,0 @@
-// const mysql = require('mysql');
-// const db = mysql.createConnection({
-//     user: process.env.DATABASE_USER || 'root',
-//     password: process.env.DATABASE_PASSWORD || '',
-//     database: process.env.DATABASE || 'cinema_management',
-//     port: process.env.DATABASE_PORT || 3306
-// });
-
-// module.exports = db;
